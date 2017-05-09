fc39b9645a611f22997e0b203554dcc4869ac0c2
Robby Barton <searontrejorek@gmail.com>
2017-05-09 18:16:49 -0400

    custom solarized colors

--- config_setup.sh
+++ config_setup.sh
@@ -9,6 +9,7 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone git://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
+cp $PWD/solarized.vim ~/.vim/bundle/vim-colors-solarized/colors/
 
 vim +PluginInstall +qall
 
