c15d0a77b57ffe9dcee8bc84b2aa265f8285cfee
Robby Barton <rbartonsc@gmail.com>
2023-01-24 12:42:39 -0500

    Add ruby and go to treesitter languages (#18)

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "go",
+        "help",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "ruby",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+      sync_install = false,
+      highlight = {
+        enable = true,
+      },
+    },
+  },
+}
