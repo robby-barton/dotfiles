227cc6d688db7ff8264d5de4ba03fb4a98be88a6
Robby Barton <rbartonsc@gmail.com>
2023-05-01 00:15:40 -0400

    install corrections, remove lazy-lock.json (#21)
    * small corrections to install file
    
    * remove lazy-lock.json from tracking
    
    * make safe copy for install script

--- install.sh
+++ install.sh
@@ -1,16 +1,31 @@
 #! /usr/bin/env bash
-PWD=`pwd`
+PWD=$(pwd)
+
+function safeCopyConfig() {
+	if [ -z "${1}" ]; then
+		return
+	fi
+
+	expectedName=~/.${1}
+	if [ -z "${2}" ]; then
+		expectedName=~/.${2}
+	fi
+	if [ -f ${expectedName} ]; then
+		mv ${expectedName} ${expectedName}.bak
+	fi
+	ln -sf $PWD/${1} ${expectedName}
+}
 
 mkdir -p ~/.config
 
 # link config files
-ln -sf $PWD/vimrc ~/.vimrc
-ln -sf $PWD/bashrc ~/.bashrc
-ln -sf $PWD/zshrc ~/.zshrc
-ln -sf $PWD/p10k.zsh ~/.p10k.zsh
-ln -sf $PWD/aliasrc ~/.aliasrc
-ln -sf $PWD/gitconfig ~/.gitconfig
-ln -sf $PWD/nvim ~/.config/nvim
+safeCopyConfig vimrc
+safeCopyConfig bashrc
+safeCopyConfig zshrc
+safeCopyConfig p10k.zsh
+safeCopyConfig aliasrc
+safeCopyConfig gitconfig
+safeCopyConfig nvim config/nvim
 
 # base16 install
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
@@ -23,7 +38,4 @@ git clone https://github.com/romkatv/powerlevel10k.git ~/.config/powerlevel10k
 mkdir -p ~/.vim/autoload
 wget -O ~/.vim/autoload/plug.vim \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim +PlugInstall +qall
-
-# neovim setup
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+\vim +PlugInstall +qall
